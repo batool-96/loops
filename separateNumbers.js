e3f15ff597da0b94a566eae75d7e444523a48658
Mattias Schertell <schertell@gmail.com>
2023-10-29 18:29:30 +0100

    Create separateNumbers.js

---
+++ separateNumbers.js
@@ -0,0 +1,23 @@
+
+//separateNumbers.js
+
+function separateNumbersForLoop(arr) {
+  const evenArr = [];
+  const oddArr = [];
+
+  //gör en for-loop som placerar jämna tal i evenArr
+  //och udda tal i oddArr
+    
+  return [evenArr, oddArr];
+}
+
+//implementera funktionerna separateNumbersWhileLoop och  separateNumbersDoWhileLoop.
+//bägge dessa funktioner ska göra samma sak som separateNumbersForLoop fast med en 
+//while-loop respektive en Do-While-loop
+  
+
+module.exports = {
+  separateNumbersForLoop,
+  separateNumbersWhileLoop,
+  separateNumbersDoWhileLoop,
+};
