cd2d5270437596fd9a924ec962293221d31e60a8
batool-96 <152862263+batool-96@users.noreply.github.com>
2023-12-13 23:42:11 +0100

    Update separateNumbers.js

--- separateNumbers.js
+++ separateNumbers.js
@@ -7,7 +7,69 @@ function separateNumbersForLoop(arr) {
 
   //gör en for-loop som placerar jämna tal i evenArr
   //och udda tal i oddArr
-    
+    function separateNumbersForLoop(array) {
+    let even = [];
+    let odd = [];
+   
+    for (let i = 0; i < array.length; i++) {
+       if (array[i] % 2 === 0) {
+         even.push(array[i]);
+       } else {
+         odd.push(array[i]);
+       }
+    }
+   
+    return [even, odd];
+   }
+   
+   function separateNumbersWhileLoop(array) {
+    let even = [];
+    let odd = [];
+    let i = 0;
+   
+    while (i < array.length) {
+       if (array[i] % 2 === 0) {
+         even.push(array[i]);
+       } else {
+         odd.push(array[i]);
+       }
+       i++;
+    }
+   
+    return [even, odd];
+   }
+   
+   function separateNumbersDoWhileLoop(array) {
+    let even = [];
+    let odd = [];
+    let i = 0;
+   
+    do {
+       if (array[i] % 2 === 0) {
+         even.push(array[i]);
+       } else {
+         odd.push(array[i]);
+       }
+       i++;
+    } while (i < array.length);
+   
+    return [even, odd];
+   }
+   
+   function printArray(array) {
+    array.forEach(element => {
+       console.log(element);
+    });
+   }
+  
+   let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+   let [evens, odds] = separateNumbersForLoop(numbers);
+   
+   console.log("Even numbers:");
+   printArray(evens);
+   
+   console.log("Odd numbers:");
+   printArray(odds);
   return [evenArr, oddArr];
 }
 
